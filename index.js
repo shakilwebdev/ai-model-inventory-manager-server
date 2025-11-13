08740f023ad605122444fafc5dff8d59d25c6041
shakilwebdev <shakilwebdeveloper1@gmail.com>
2025-11-13 15:13:48 +0600

    ai-model-inventory-manager-server

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+// usersName=aiModel-db
+// password=r98SgGNO8cz5QgWm
+
+const uri =
+  "mongodb+srv://aiModel-db:r98SgGNO8cz5QgWm@cluster0.s1ftcag.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("aiModel-db");
+    const modelCollection = db.collection("models");
+    // find
+    // findOne
+    app.get("/models", async (req, res) => {
+      const result = await modelCollection.find().toArray();
+      // console.log(result);
+      res.send(result);
+    });
+
+    // post method
+    app.post("/models", async (req, res) => {
+      const data = req.body;
+      // console.log(data);
+      const result = await modelCollection.insertOne(data);
+      res.send(result);
+      /*       res.send({
+        success: true,
+        result,
+      }); */
+      /*       res.send({
+        message: "Data received successfully",
+      }); */
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
